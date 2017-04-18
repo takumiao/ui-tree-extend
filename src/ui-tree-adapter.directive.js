730cd32f399756a3e5cc0a2f1c211841cc22aa25
takumiao <takumiao@gmail.com>
2017-04-18 13:25:25 +0800

    add show-root attribute for ui-tree-adapter

--- src/ui-tree-adapter.directive.js
+++ src/ui-tree-adapter.directive.js
@@ -8,8 +8,17 @@ angular
         var idAttr = tAttrs.idAttr || 'id';
         var parentAttr = tAttrs.parentAttr || 'pid';
         var childrenAttr = 'nodes';
+      
         return function postLink(scope, elem, attrs, ngModelCtrl) {
           var rootId = angular.isDefined(attrs.rootId) ? $parse(attrs.rootId)(scope) : null;
+          var showRoot = false;
+
+          // Add Boolean support
+          if (angular.isDefined(tAttrs.showRoot)) {
+            showRoot = $parse(tAttrs.showRoot)(scope);
+            if (showRoot === void 0) showRoot = true;
+          }
+
           scope.$data = [];
           scope.$add = function(node) {
             ngModelCtrl.$modelValue.push(node);
@@ -27,12 +36,17 @@ angular
           scope.$watch(attrs.rootId, function(newValue, oldValue) {
             if (newValue === oldValue) return;
             rootId = newValue;
-            scope.$data = treeify(ngModelCtrl.$modelValue, rootId, idAttr, parentAttr, childrenAttr);
-          })
+            scope.$data = treeify(ngModelCtrl.$modelValue, rootId, idAttr, parentAttr, childrenAttr, showRoot);
+          });
+          scope.$watch(attrs.showRoot, function(newValue, oldValue) {
+            if (newValue === oldValue) return;
+            showRoot = !!newValue;
+            scope.$data = treeify(ngModelCtrl.$modelValue, rootId, idAttr, parentAttr, childrenAttr, showRoot);
+          });
           scope.$watchCollection(function() {
             return ngModelCtrl.$modelValue
           }, function(newValue, oldValue) {
-            scope.$data = treeify(newValue, rootId, idAttr, parentAttr, childrenAttr);
+            scope.$data = treeify(newValue, rootId, idAttr, parentAttr, childrenAttr, showRoot);
           });
 
           $timeout(function() {
@@ -44,8 +58,7 @@ angular
               var sortedIndex = -1;
               // adjust parent of node
               sourceNodeScope.$modelValue.pid = destNodeScope.$nodeScope ? destNodeScope.$nodeScope.$modelValue.id : rootId
-                // adjust original pos of flatten nodes by sorted nodes           
-                // #bug convert structure JSON
+              // adjust original pos of flatten nodes by sorted nodes           
               for (var i = 0, l = ngModelCtrl.$modelValue.length; i < l; i++) {
                 var node = ngModelCtrl.$modelValue[i];
                 if (node.pid == sourceNodeScope.$modelValue.pid) {
@@ -53,25 +66,25 @@ angular
                   node !== sortedNode && ngModelCtrl.$modelValue.splice(i, 1, sortedNode);
                 }
               }
-              console.log(sourceNodeScope.$modelValue, '->', destNodeScope);
               dragStop(event);
             }
           });
 
           // http://stackoverflow.com/questions/22367711/construct-hierarchy-tree-from-flat-list-with-parent-field
-          function treeify(list, rootId, idAttr, parentAttr, childrenAttr) {
+          function treeify(list, rootId, idAttr, parentAttr, childrenAttr, showRoot) {
             var treeList = [];
             var lookup = {};
             list.forEach(function(obj) {
               lookup[obj[idAttr]] = obj;
               obj[childrenAttr] = [];
             });
+
             list.forEach(function(obj) {
-              if (obj[parentAttr] != rootId) {
+              if (obj[showRoot ? idAttr : parentAttr] == rootId) {
+                treeList.push(obj);
+              } else {
                 var parent = lookup[obj[parentAttr]]
                 parent && parent[childrenAttr].push(obj);
-              } else {
-                treeList.push(obj); // build root node
               }
             });
             return treeList;
